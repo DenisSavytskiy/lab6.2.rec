c17693a7c1e346e1b25bf9bed12cb95094e8ba87
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-15 01:53:41 +0300

    first commit

---
+++ AP_Lab№6.2.рек/AP_Lab№6.2.рек.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <climits>
+using namespace std;
+
+void generateArray(int a[], int size, int index = 0) {
+    if (index == size) return;
+    a[index] = rand() % 46 - 10;
+    generateArray(a, size, index + 1);
+}
+
+void printArray(const int a[], int size, int index = 0) {
+    if (index == size) {
+        cout << endl;
+        return;
+    }
+    cout << a[index] << " ";
+    printArray(a, size, index + 1);
+}
+
+int findElement(const int a[], const int size, int index = 0, int smallest = 36, int indexMin = -1) {
+    if (index == size) return indexMin;
+
+    if (a[index] <= smallest && a[index] % 2 == 0) {
+        smallest = a[index];
+        indexMin = index;
+    }
+
+    return findElement(a, size, index + 1, smallest, indexMin);
+}
+
+void replaceElements(int a[], int indexMin) {
+    if (indexMin != -1) {
+        a[indexMin] = 0;
+    }
+}
+
+int main() {
+    const int SIZE = 22;
+    srand((unsigned)time(NULL));
+    int a[SIZE];
+    generateArray(a, SIZE);
+    printArray(a, SIZE);
+    int indexMin = findElement(a, SIZE);
+    if (indexMin != -1) {
+        cout << "Smallest element index: " << indexMin << endl;
+        replaceElements(a, indexMin);
+        printArray(a, SIZE);
+    }
+    else {
+        cout << "No even elements found" << endl;
+    }
+    
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№6.2.рек/AP_Lab№6.2.рек.cpp"
+
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestGenerateArray)
+        {
+            const int size = 10;
+            int a[size];
+            generateArray(a, size);
+
+            for (int i = 0; i < size; i++) {
+                Assert::IsTrue(a[i] >= -10 && a[i] <= 35);
+            }
+        }
+
+        TEST_METHOD(TestFindElement)
+        {
+            const int size = 5;
+            int a[size] = { 1, 2, 3, 4, 5 };
+            int index = findElement(a, size, 0, 6, -1);
+            Assert::AreEqual(1, index);
+        }
+
+        TEST_METHOD(TestReplaceElements)
+        {
+            int index = 1;
+            int a[5] = { 1, 2, 3, 4, 5 };
+            replaceElements(a, index);
+
+            Assert::AreEqual(1, a[0]);
+            Assert::AreEqual(0, a[1]);
+            Assert::AreEqual(3, a[2]);
+            Assert::AreEqual(4, a[3]);
+            Assert::AreEqual(5, a[4]);
+        }
+    };
+}
